9aa7d491532986dd8a4013e64a39343d96b31b3e
PC <PC@DESKTOP-LE16GCH>
2022-04-28 14:42:05 +0800

    Second commit

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,32 @@
 
 public class Student {
+	
+	private String name;
+	private String id;
+	
+	public Student(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String toString() {
+		return name + id;
+	}
 
+		public static void main(String[]args) {
+			
+			Student s1 = new Student("2016-1123 ", "Paragele, Tracy");
+			
+			System.out.println("Student Name: " + s1);
+			
+		}		
 }
+
